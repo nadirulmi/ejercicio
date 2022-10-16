3b6f21d2b611799cd7e6acaf4ed66d9c874bb468
“nadirulmi” <ulminadir99@gmail.com>
2022-10-16 13:49:51 -0300

    ejercicio DOM

---
+++ script.js
@@ -0,0 +1,25 @@
+let titulo = document.querySelector(".tituloVentas");
+titulo.style = "color:purple; text-align:center";
+titulo.style.fontSize="40px";
+titulo.textContent = "Venta de computadoras";
+
+
+let subtitulo = document.querySelector("#titulo");
+subtitulo.textContent = "Notebook Asus";
+subtitulo.style = "text-align:center";
+
+let contacto = document.querySelector("#contacto");
+contacto.textContent = "Comprar";
+contacto.style = "color:black; display:flex; justify-content: center; align-items: center";
+
+let parrafito = document.querySelector(".parrafo");
+parrafito.textContent = "Notebook Asus 10254, ideal para regalar a mamá por este día de las madres! Hace tu compra hoy y obtendras un 20% de descuento";
+parrafito.style = "text-align:center";
+
+let seccion = document.querySelector(".izq");
+seccion.style = "display: flex;flex-direction: column;justify-content: center;;align-items: center";
+
+let boton = document.querySelector("#boton");
+boton.style.margin = "20px";
+
+
